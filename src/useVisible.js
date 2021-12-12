234064e4698697322d30f8cc0840edfa81f618a8
Solo-steven <ssteven19@kimo.com>
2021-12-12 16:29:24 +0800

    Feat: Init Commit with First Version Hook

---
+++ src/useVisible.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const useVisible = (target, threshold = 0.55) => {
+    const [flag, setFlag] = useState(false);
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+                (entries, observer) => {
+                    for(const entry of entries) {
+                        if(entry.isIntersecting === true) {
+                            setFlag(true);
+                        }else {
+                            setFlag(false);
+                        }
+                    }
+                }, 
+                { root: null, threshold }
+            )
+        if (target)
+            observer.observe(target);
+        return () => {
+            if (target)
+                observer.unobserve(target);
+            observer.disconnect();
+        }
+    },[setFlag, threshold, target]);
+    return flag;
+}
+
+export default useVisible;
